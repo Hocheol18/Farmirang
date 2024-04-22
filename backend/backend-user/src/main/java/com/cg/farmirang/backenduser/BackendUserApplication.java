4fd95bb3790e30c689e8dd313978b36f61fead27
이병창 <dig04214@naver.com>
2024-04-22 18:08:36 +0900

    Feat: Initial User

---
+++ backend/backend-user/src/main/java/com/cg/farmirang/backenduser/BackendUserApplication.java
@@ -0,0 +1,13 @@
+package com.cg.farmirang.backenduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUserApplication.class, args);
+	}
+
+}
