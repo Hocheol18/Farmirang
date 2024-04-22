1c0fc80dc514d2d432dbd4bf006b8a2835db39e3
김현종 <cowkey206@gmail.com>
2024-04-22 09:27:32 +0900

    Init : Farm 프로젝트 생성

---
+++ backend/backend-farm/src/main/java/com/cg/farmirang/backendfarm/BackendFarmApplication.java
@@ -0,0 +1,13 @@
+package com.cg.farmirang.backendfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFarmApplication.class, args);
+	}
+
+}
