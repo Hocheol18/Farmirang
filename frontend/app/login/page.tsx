e198c85ea6094f466e193b8f1773ae349cf0ef03
김현지 <hanguel97@gmail.com>
2024-04-29 16:12:18 +0900

    fix : 다른 브랜치에서 작업했던 거 실수 수정

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,38 +1,38 @@
 import Image from "next/image";
-import "./style.css";
+// import "./style.css";
 
 export default function Login() {
   return (
-    <div>
-      <div className="screen">
-        <div className="group">
-          <div className="rectangle-4" />
-        </div>
+    <div className="flex flex-col items-center justify-center gap-[10px] relative">
+      <div className="relative self-stretch w-full h-full bg-[url(/user/farm.jpg)] bg-cover bg-[50%_50%]">
+        <div className="w-full h-[900px] bg-[#3a3a3a9e]" />
+      </div>
 
-        <div className="frame-4">
-          <div className="text-wrapper-6">로그인 · 회원가입</div>
-          <p className="p">
-            파종부터 수확까지
-            <br />
-            나만의 주말농장 조력자, 팜이랑
-          </p>
-          <div className="frame-5">
-            <Image
-              src="/user/loginWithKaKao.png"
-              alt="kakao Logo"
-              className="KAKAO-LOGIN"
-              width={302}
-              height={45}
-            />{" "}
-            <Image
-              src="/user/loginWithGoogle2.png"
-              alt="Google Logo"
-              className="img"
-              width={302}
-              height={45}
-              priority
-            />
-          </div>
+      <div className="flex flex-col w-[23.75rem] items-center justify-center gap-[25px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="self-stretch mt-[-1.00px] text-yellow-200 text-[length:var(--t-h1-font-size)] text-center tracking-[var(--t-h1-letter-spacing)] leading-[var(--t-h1-line-height)] relative font-t-h1 font-[number:var(--t-h1-font-weight)] [font-style:var(--t-h1-font-style)]">
+          로그인 · 회원가입
+        </div>
+        <p className="self-stretch text-gray-200 text-[length:var(--t-h3-font-size)] text-center tracking-[var(--t-h3-letter-spacing)] leading-[var(--t-h3-line-height)] relative font-t-h3 font-[number:var(--t-h3-font-weight)] [font-style:var(--t-h3-font-style)]">
+          파종부터 수확까지
+          <br />
+          나만의 주말농장 조력자, 팜이랑
+        </p>
+        <div className="inline-flex flex-col h-[101px] items-center justify-center gap-[12px] relative">
+          <Image
+            src="/user/KAKAO_LOGIN.png"
+            alt="kakao Logo"
+            className="KAKAO-LOGIN"
+            width={302}
+            height={45}
+          />{" "}
+          <Image
+            src="/user/GOOGLE_LOGIN.png"
+            alt="Google Logo"
+            className="img"
+            width={302}
+            height={45}
+            priority
+          />
         </div>
       </div>
     </div>
