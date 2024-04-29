68488d599b4621e5c7c6a3d3a45a930a77303491
ParkHocheol <didifia9850@naver.com>
2024-04-29 13:20:33 +0900

    [Feat] FE : Theme 색 업데이트 및 Navbar 생성

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,6 +13,36 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        green: {
+          100: "#F3FAF3",
+          200: "#C9E9CC",
+          300: "#6DBB76",
+          400: "#388140",
+          500: "#29522E",
+        },
+        gray: {
+          100: "#F1F4F1",
+          200: "#EAECEA",
+          300: "#DDDEDD",
+          350: "#B8BCB8",
+          400: "#979A97",
+          500: "#49554A",
+        },
+        black: {
+          100: "#0F2413",
+        },
+        white: {
+          100: "#FFFFFF",
+        },
+        yellow: {
+          100: "#FFF3C2",
+          200: "#FFCB3C",
+        },
+        brown: {
+          100: "#723511",
+        },
+      },
     },
   },
   plugins: [],
