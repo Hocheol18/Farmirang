7c76cdf417c0684a019695c1a3db65f5c4134820
김현지 <hanguel97@gmail.com>
2024-05-09 09:18:07 +0900

    feat: 커뮤니티 네브바 우선 연결

--- frontend/app/board/barter/page.tsx
+++ frontend/app/board/barter/page.tsx
@@ -4,8 +4,8 @@ export default function Barter() {
   return (
     <div className="flex flex-col justify-center mx-auto gap-[30px]">
       {/* 상단 현재 위치 */}
-      <div className="flex items-center justify-center">밭 꾸미기</div>
-      <MiniNavigation status={"neighbor"} />
+      <div className="flex items-center justify-center">작물교환</div>
+      <MiniNavigation status={"barter"} />
       {/* 내용 */}
       <div className="inline-flex flex-col items-start justify-center gap-[15px] p-[15px] relative flex-[0_0_auto] border !rounded-[10px] border-solid border-graygray-300  min-w-[1220px]">
         내용
