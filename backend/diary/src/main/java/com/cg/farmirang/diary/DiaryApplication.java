c7d3d60765a2a875f6353fbbf00aa0f3eff37306
김현종 <cowkey206@gmail.com>
2024-05-10 13:23:37 +0900

    Feat : cors 기능 추가

--- backend/diary/src/main/java/com/cg/farmirang/diary/DiaryApplication.java
+++ backend/diary/src/main/java/com/cg/farmirang/diary/DiaryApplication.java
@@ -2,6 +2,9 @@ package com.cg.farmirang.diary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DiaryApplication {
@@ -9,5 +12,18 @@ public class DiaryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DiaryApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOriginPatterns("*")
+					.allowedHeaders("*")
+					.exposedHeaders("*")
+					.allowedMethods("*")
+					.allowCredentials(true);
+			}
+		};
+	}
 }
