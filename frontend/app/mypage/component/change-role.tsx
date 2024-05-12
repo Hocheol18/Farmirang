a0426a47b6cd38c2b5c91edd3232e7d0c1d739d4
김현지 <hanguel97@gmail.com>
2024-05-13 06:21:50 +0900

    feat: 기관신청 작성중

--- frontend/app/mypage/component/change-role.tsx
+++ frontend/app/mypage/component/change-role.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { MEMBER_URL } from "@/utils/ServerApi";
 import Input from "@/app/_components/common/Input";
 import DaumPost from "@/app/_components/common/address";
+import ImageComponent from "@/app/_components/common/Image";
+import { useUserStore } from "@/app/_stores/userStore";
 
 interface Props {
   areaAddress: string;
@@ -8,10 +11,38 @@ interface Props {
 }
 
 export default function ChangeRole() {
+  const { userInfo } = useUserStore();
+  const [selectImage, setSelectImage] = useState<any>();
+  const [name, setName] = useState<string>("");
+  const [detailedAddress, setDetailedAddress] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [report, setReport] = useState<string>("");
+  const [contact, setContact] = useState<string>("");
   const [addressObj, setAddressObj] = useState<Props>({
     areaAddress: "",
     townAddress: "",
   });
+
+  // 기관등록 신청 로직
+  const postAgency = async () => {
+    const formData = new FormData();
+    formData.append("image", selectImage);
+    const response = await fetch(`${MEMBER_URL}/v1/agency/registration`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${userInfo.accessToken}`,
+      },
+      body: formData,
+    });
+    if (response) {
+      const responseData = await response.json();
+    }
+  };
+
+  // 주소 한줄로 합치는 핸들러
+  const handleAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAddress(e.target.value);
+  };
 
   return (
     <>
@@ -22,23 +53,12 @@ export default function ChangeRole() {
         }
         placeholder={"기관 또는 단체의 공식 명칭을 입력하세요."}
         type={"string"}
-        value={undefined}
+        value={name}
         topcss={"mt-10"}
         labeltext={"시설명"}
-        onChange={() => {}}
+        onChange={(value) => setName(value)}
       />
-      <Input
-        labelcss={"text-lg font-semibold"}
-        inputcss={
-          "flex rounded-lg border border-green-300 w-full focus:outline-none focus:ring-green-400 focus:ring-1 h-10 p-2"
-        }
-        placeholder={"기관 또는 단체가 위치한 정확한 주소를 입력하세요."}
-        type={"string"}
-        value={undefined}
-        topcss={"mt-10"}
-        labeltext={"소재지"}
-        onChange={() => {}}
-      />
+      <div className="text-lg font-semibold mt-10">소재지</div>
       <DaumPost setAddressObj={setAddressObj} />
       <div className="mt-4">
         <input
@@ -58,8 +78,9 @@ export default function ChangeRole() {
       </div>
       <div className="mt-4">
         <input
-          type="text"
-          onChange={() => {}}
+          type="string"
+          value={detailedAddress}
+          onChange={(e) => setDetailedAddress(e.target.value)}
           className="flex rounded-lg border border-green-300 w-full focus:outline-none focus:ring-green-400 focus:ring-1 h-10 p-2"
           placeholder="상세 주소를 입력해주세요"
         />
@@ -71,10 +92,10 @@ export default function ChangeRole() {
         }
         placeholder={"복지시설 신고번호를 입력하세요."}
         type={"string"}
-        value={undefined}
+        value={report}
         topcss={"mt-10"}
         labeltext={"복지시설신고번호"}
-        onChange={() => {}}
+        onChange={(e) => setReport(e)}
       />
       <Input
         labelcss={"text-lg font-semibold"}
@@ -83,26 +104,21 @@ export default function ChangeRole() {
         }
         placeholder={"연락 가능한 전화번호를 입력하세요."}
         type={"string"}
-        value={undefined}
+        value={contact}
         topcss={"mt-10"}
         labeltext={"전화번호"}
-        onChange={() => {}}
+        onChange={(e) => setContact(e)}
       />
-      <Input
-        labelcss={"text-lg font-semibold"}
-        inputcss={
-          "flex rounded-lg border border-green-300 w-full focus:outline-none focus:ring-green-400 focus:ring-1 h-10 p-2"
-        }
-        placeholder={
-          "복지시설 신고를 위한 공식 증명을 첨부해주세요. (jpg, jpeg, png 형식만 첨부)"
-        }
-        type={"string"}
-        value={undefined}
-        topcss={"mt-10"}
-        labeltext={"시설신고증사진"}
-        onChange={() => {}}
+      <div className="text-lg font-semibold mt-10">시설 신고증 사진</div>
+      <ImageComponent
+        title={"복지시설 신고를 위한 공식 증명을 첨부해주세요"}
+        titlecss={""}
+        topcss={""}
+        topsecondcss={""}
+        displayImage={selectImage}
+        setDisplayImage={setSelectImage}
       />
-      <div className="text-gray-400 text-[13px] font-m-6 leading-[18px]">
+      <div className="text-gray-400 text-[13px] font-m-6 leading-[18px] mt-[10px]">
         복지시설 확인을 위한 용도로 별도 저장되지 않습니다
       </div>
     </>
