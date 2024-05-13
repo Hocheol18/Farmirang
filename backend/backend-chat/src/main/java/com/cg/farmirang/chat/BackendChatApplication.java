447f9fa93cac0597c1a1f59882730774d2605764
김지은 <jieun9912@gmail.com>
2024-05-13 22:59:22 +0900

    Feat : @EnableFeignClients 추가해서 JwtClient 빈 생성

--- backend/backend-chat/src/main/java/com/cg/farmirang/chat/BackendChatApplication.java
+++ backend/backend-chat/src/main/java/com/cg/farmirang/chat/BackendChatApplication.java
@@ -2,8 +2,11 @@ package com.cg.farmirang.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BackendChatApplication {
 
     public static void main(String[] args) {
