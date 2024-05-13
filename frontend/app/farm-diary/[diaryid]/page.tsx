1714b9acbe02060dd91c5b2dd33e4e7fb60cf7c0
Hocheol <didifia9850@naver.com>
2024-05-13 10:36:21 +0900

    [feat] FE : 캘린더 업데이트

--- frontend/app/farm-diary/[diaryid]/page.tsx
+++ frontend/app/farm-diary/[diaryid]/page.tsx
@@ -1,3 +1,8 @@
-export default function Diary() {
-  return "";
-}
+import { FetchCalendar } from "@/api/farm_diary";
+
+const Posts = async () => {
+  const data = await FetchCalendar();
+  return data.data.result.map((item : any, index : number) => <div key={index}>{JSON.stringify(item)}</div>);
+};
+
+export default Posts;
--- frontend/utils/ServerApi.tsx
+++ frontend/utils/ServerApi.tsx
@@ -1,3 +1,5 @@
 export const BASE_URL = "http://k10a105.p.ssafy.io:8080";
 
 export const MEMBER_URL = "http://k10a105.p.ssafy.io:8081/api";
+
+export const FARM_DIARY = "http://k10a105.p.ssafy.io:8082/api"
