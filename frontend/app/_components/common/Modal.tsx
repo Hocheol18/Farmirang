0f8c7af1c0ed7bca0c64352e07c054d3a3815bbd
ParkHocheol <didifia9850@naver.com>
2024-05-16 17:41:06 +0900

    [fix] FE : 버그 수정

--- frontend/app/_components/common/Modal.tsx
+++ frontend/app/_components/common/Modal.tsx
@@ -131,7 +131,7 @@ export default function MyModal({
                       <button
                         type="button"
                         className="inline-flex justify-center rounded-md shadow-xl border border-transparent bg-green-400 px-4 py-2 cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
-                        onClick={handleFunction}
+                        onClick={onSuccess}
                       >
                         <div className="text-white-100 font-bold">{next}</div>
                       </button>
