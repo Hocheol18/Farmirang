834d6e3335671a3721372638b75121df04dec904
김현종 <cowkey206@gmail.com>
2024-05-17 18:56:16 +0900

    Fix : 수정 build.gradle 수정 service.yaml

--- backend/diary/src/main/java/com/cg/farmirang/diary/DiaryApplication.java
+++ backend/diary/src/main/java/com/cg/farmirang/diary/DiaryApplication.java
@@ -2,13 +2,12 @@ package com.cg.farmirang.diary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class DiaryApplication {
 
 	public static void main(String[] args) {
